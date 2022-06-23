b549e83e125e32b781484a66a7838a93b01482e2
DevExpressExampleBot <fakeEmail>
2022-06-23 17:28:24 +0400

    Source auto update [skip ci]

--- VB/dxSample/MainWindow.xaml.vb
+++ VB/dxSample/MainWindow.xaml.vb
@@ -1,36 +1,24 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 
 Namespace dxSample
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		End Sub
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
 
-		Private Sub item_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
-			Me.dxDockLayoutManager.MDIController.TileHorizontal(Me.dxDocumentGroup)
-		End Sub
+        Private Sub item_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
+            Me.dxDockLayoutManager.MDIController.TileHorizontal(Me.dxDocumentGroup)
+        End Sub
 
-		Private Sub item_ItemClick_1(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
-			Me.dxDockLayoutManager.MDIController.TileVertical(Me.dxDocumentGroup)
-		End Sub
-	End Class
+        Private Sub item_ItemClick_1(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
+            Me.dxDockLayoutManager.MDIController.TileVertical(Me.dxDocumentGroup)
+        End Sub
+    End Class
 End Namespace
